88312f195fc71905ac2f12ee76fedcd244f25012
matthew murphy <mattyhead@gmail.com>
2019-05-09 16:27:19 -0400

    ...

--- admin/models/workers.php
+++ admin/models/workers.php
@@ -58,7 +58,7 @@ class PvdemosModelWorkers extends JModel
     {
         d('in buildQuery');
         $where = ' ';
-        $query = 'SELECT * FROM `#__pvdemos` ';
+        $query = 'SELECT * FROM `#__pvdemos_workers` ';
 
         return $query . $where;
     }
