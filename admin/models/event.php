9e3401d57b64593202012271b0ec3e839f4e7236
matthew murphy <mattyhead@gmail.com>
2019-05-15 16:22:35 -0400

    ...

--- admin/models/event.php
+++ admin/models/event.php
@@ -71,8 +71,8 @@ class PvdemosModelEvent extends JModel
         $dateNow = &JFactory::getDate();
         $dateIndex = $this->_id ? 'updated' : 'created';
 
-        $data['start'] = date("Y-m-d H:i:s", strtotime($data['day'] . " " . $data['start_hour'] . ":" . $data['start_minute'] . ":" . "00"));
-        $data['end'] = date("Y-m-d H:i:s", strtotime($data['day'] . " " . $data['end_hour'] . ":" . $data['end_minute'] . ":" . "00"));
+        $data['start'] = date("Y-m-d H:i:s", strtotime($data['day'] . " " . ( $data['start_am_pm'] == 'PM' && $data['start_hour'] != 12 ? $data['start_hour']+12 : $data['start_hour'] ). ":" . $data['start_minute'] . ":" . "00"));
+        $data['end'] = date("Y-m-d H:i:s", strtotime($data['day'] . " " . ( $data['end_am_pm'] == 'PM' && $data['end_hour'] != 12 ? $data['end_hour']+12 : $data['end_hour'] ). ":" . $data['end_minute'] . ":" . "00"));
 dd($data);
         foreach ($data as $key => $value) {
             $data[$key] = JString::trim($value);
