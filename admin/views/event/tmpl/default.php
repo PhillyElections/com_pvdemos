09dbee4d36c60b62e62902a45114771c768cbc68
matthew murphy <mattyhead@gmail.com>
2019-05-16 15:27:00 -0400

    ...

--- admin/views/event/tmpl/default.php
+++ admin/views/event/tmpl/default.php
@@ -316,6 +316,7 @@ endif;
 <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 <script src="//unpkg.com/leaflet@1.3.1/dist/leaflet.js"></script>
+<script src="components/com_voterapp/assets/js/leaflet.print.js"></script>
 <script src="https://unpkg.com/esri-leaflet@2.1.3/dist/esri-leaflet.js"></script>
 <script src='https://npmcdn.com/@turf/turf/turf.min.js'></script>
 <script type="text/javascript" src="components/com_pvdemos/assets/js/main.js"></script>
