e8d0ea53dc4b6f29f7c5224503fc74cbdb1498d0
matthew murphy <mattyhead@gmail.com>
2019-05-17 14:33:54 -0400

    ...

--- admin/assets/js/main.js
+++ admin/assets/js/main.js
@@ -425,6 +425,7 @@ console.log('in addressEntered');
             }
         }).then(function(){
           console.log(Markers.home._latlng)
+          map.setView(Markers.home._latlng, 8)
         }).fail(invalidAddress);
     }
 
