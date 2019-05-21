383d5fa1444e56844cf7891f0a1e5c0b14ec4ddf
matthew murphy <mattyhead@gmail.com>
2019-05-21 11:53:31 -0400

    ...

--- admin/views/event/tmpl/default.php
+++ admin/views/event/tmpl/default.php
@@ -67,7 +67,7 @@ foreach ($this->events as $element) {
     }
     $counter++;
 }
-s($events);
+
 $document = JFactory::getDocument();
 
 $document->addStyleSheet('//unpkg.com/leaflet@1.3.1/dist/leaflet.css');
@@ -83,11 +83,12 @@ $document->addStyleSheet('components/com_pvdemos/assets/css/main.css');
   <div class="right fifty-five">
     <input name="target" id="target" placeholder="TARGET PLACEHOLDER"/>
     <div id="map-canvas"></div>
-    <p>Click to place a custom marker / override geocoding:
+<!--     <p>Click to place a custom marker / override geocoding:
       <ul class="markers">
         <li class="marker" id="building"><img src="components/com_pvdemos/assets/images/b.png" /> <?=JText::_('BUILDING');?></li>
       </ul>
     </p>
+ -->
   </div>
   <div class="left">
 <?php
